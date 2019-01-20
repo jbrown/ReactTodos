bf157505a90309311108eee0430753ceec045cf3
Justin Brown <justinbrown.work@gmail.com>
2019-01-20 15:29:49 -0600

    Create a TodoItem component and render it from Todos.

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <h3>{this.props.todo.title}</h3>
+    );
+  }
+}
+
+export default TodoItem;
