3bf3da973825021c39eccd51400491b31fa6bbf6
Justin Brown <justinbrown.work@gmail.com>
2019-01-20 18:13:36 -0600

    Add styling to TodoItem and ability to toggle completed.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      backgroundColor: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    };
+  }
+
   render() {
+    let { completed, id, title } = this.props.todo;
+
     return (
-      <h3>{this.props.todo.title}</h3>
+      <div style={this.getStyle()}>
+        <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> { ' ' }
+        {title}
+      </div>
     );
   }
 }
